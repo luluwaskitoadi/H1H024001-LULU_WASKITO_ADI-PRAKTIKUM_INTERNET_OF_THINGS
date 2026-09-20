cf4d7fece74798c913b6b67a2b0773747dacd625
Lulu Waskito Adi <luluwaskitoadi@gmail.com>
2026-09-20 22:46:32 +0700

    Add files via upload

---
+++ Pertemuan 3 - Protokol Komunikasi IoT/code/percobaan3a_komunikasi_http_modifikasi.ino
@@ -0,0 +1,57 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+
+const char* ssid = "NAMA_WIFI_ANDA";
+const char* password = "PASSWORD_WIFI_ANDA";
+const char* serverUrl = "https://httpbin.org/post";
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+
+  Serial.print("Menghubungkan ke WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.println("WiFi berhasil terhubung!");
+}
+
+void loop() {
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+    http.begin(serverUrl);
+    http.addHeader("Content-Type", "application/json");
+
+    // Membuat objek data sensor dalam format JSON
+    JsonDocument doc;
+    doc["suhu"] = 28.5;
+    doc["kelembaban"] = 65.0;
+    doc["waktu_ms"] = millis(); // data tambahan: waktu sejak ESP32 dinyalakan (ms)
+
+    String requestBody;
+    serializeJson(doc, requestBody);
+
+    Serial.print("Mengirim data: ");
+    Serial.println(requestBody);
+
+    // Mengirim data melalui HTTP POST
+    int httpResponseCode = http.POST(requestBody);
+
+    if (httpResponseCode > 0) {
+      Serial.print("Kode Response HTTP: ");
+      Serial.println(httpResponseCode);
+      Serial.println("Isi Response:");
+      Serial.println(http.getString());
+    } else {
+      Serial.print("Pengiriman gagal, kode error: ");
+      Serial.println(httpResponseCode);
+    }
+
+    http.end();
+  }
+
+  delay(10000); // kirim data setiap 10 detik
+}
